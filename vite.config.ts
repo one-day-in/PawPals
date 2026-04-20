ad96e71c8ee89bbe1580efc384f75ec69ed958f3
ARTEM KOTOSHCHUK <skabullartem@gmail.com>
2026-04-20 20:24:30 +0700

    Setup GitHub Actions Pages deploy and use relative build base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/PawPals/' : '/',
+  base: command === 'build' ? './' : '/',
   build: {
     rollupOptions: {
       input: {
